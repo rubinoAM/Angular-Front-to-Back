28c3b38651dd6ae6242c804d7b7aea2fe1619126
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-18 14:50:53 -0400

    Typescript Notes

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //What tag you put into index.html to render this component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- typescript101/ex1.ts
+++ typescript101/ex1.ts
@@ -3,5 +3,3 @@ let myName: string = 'Michael Rubino';
 function addNumbers(num1: number, num2: number): number{
     return num1 + num2;
 }
-
-console.log(addNumbers(3,3));
