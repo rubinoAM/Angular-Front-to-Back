94f0126031ea8815e3c0b4cd36c9409b5aa775fb
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-18 15:11:21 -0400

    Beginning properties and methods

--- angular-app/src/app/components/user/user.component.ts
+++ angular-app/src/app/components/user/user.component.ts
@@ -2,10 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    //template: '<h2>Michael Rubino</h2>'
+    //template: '<h2>Michael Rubino</h2>',
+    /*styles: [
+        h2{
+            color: chartreuse;
+        }
+    ],*/
     templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
 })
 
 export class UserComponent {
+    //Properties
+    firstName:string = 'Michael';
+    lastName:string = 'Rubino';
+    age:number = 27;
 
+    //Methods
+    constructor(){
+        console.log('Howdy, User!');
+    }
 }
