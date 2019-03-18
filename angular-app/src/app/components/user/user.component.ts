aedab13e7f74af989e86a402f51da110e319c226
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-18 15:06:04 -0400

    Switched to templateUrl for UserComponent

--- angular-app/src/app/components/user/user.component.ts
+++ angular-app/src/app/components/user/user.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    template: '<h2>Michael Rubino</h2>'
+    //template: '<h2>Michael Rubino</h2>'
+    templateUrl: './user.component.html',
 })
 
 export class UserComponent {
-    
+
 }
