a3abd54e8aa5f0b1eb4e008b3c81c4d9821a4d93
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-18 15:33:21 -0400

    Using types with properties and methods

--- angular-app/src/app/components/user/user.component.ts
+++ angular-app/src/app/components/user/user.component.ts
@@ -14,21 +14,39 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     //Properties
-    firstName:string = 'Michael';
-    lastName:string = 'Rubino';
-    age:number = 27;
-    address = {
-        street: '62 E 2nd St.',
-        city: 'Freeport',
-        state: 'NY',
-    }
+    firstName:string;
+    lastName:string;
+    age:number;
+    address;
+    foo:any;
+    hasKids:boolean;
+    numberArray:number[];
+    stringArray:string[];
+    mixedArray:any[];
+    myTuple:[string,number,boolean];
 
     //Methods
     constructor(){
-        this.sayHello()
+        this.firstName = 'Michael';
+        this.lastName = 'Rubino';
+        this.age = 27;
+        this.address = {
+            street: '62 E 2nd St.',
+            city: 'Freeport',
+            state: 'NY',
+        }
+        this.foo = 4;
+        this.hasKids = false;
+        this.numberArray = [4,10,3,6];
+        this.stringArray = ['dog','fish','panda'];
+        this.mixedArray = [false, 0, undefined, 'dodo'];
+        this.myTuple = ['tuna',15,true]; //You can put more stuff in after the last condition has been met. Everything before that has to match, though.
+
+        this.sayHello();
         /* console.log(this.age)
         this.hasBirthday()
         console.log(this.age) */
+        console.log(this.addNumbers(1,1));
     }
 
     sayHello(){
@@ -37,5 +55,9 @@ export class UserComponent {
 
     hasBirthday(){
         this.age += 1;
+    }
+
+    addNumbers(num1:number, num2:number): number{
+        return num1 + num2
     }
 }
