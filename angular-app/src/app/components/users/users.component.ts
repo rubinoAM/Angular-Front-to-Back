f67081384bb76fa1f9a07216b07bdd1e86de4e1e
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-19 13:21:29 -0400

    ngFor on Users

--- angular-app/src/app/components/users/users.component.ts
+++ angular-app/src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(){
 
-  ngOnInit(){ //Runs automatically when constructor is initialized
-    console.log("OY!")
   }
 
+  ngOnInit(){ //Runs automatically when constructor is initialized
+    this.users = [
+      {
+        firstName:'Michael',
+        lastName:'Rubino',
+        age:27,
+        address:{
+            street:'62 E Second St.',
+            city:'Freeport',
+            state:'NY',
+            zip:11520,
+        },
+      },
+      {
+        firstName:'Lisa',
+        lastName:'Rubino',
+        age:31,
+        address:{
+            street:'62 E Second St.',
+            city:'Freeport',
+            state:'NY',
+            zip:11520,
+        },
+      },
+      {
+        firstName:'Christopher',
+        lastName:'Rubino',
+        age:49,
+        address:{
+            street:'62 E Second St.',
+            city:'Freeport',
+            state:'NY',
+            zip:11520,
+        },
+      },
+      {
+        firstName:'Kimberly',
+        lastName:'Rubino',
+        age:45,
+        address:{
+            street:'62 E Second St.',
+            city:'Freeport',
+            state:'NY',
+            zip:11520,
+        },
+      },
+    ];
+
+    this.addUser({
+      firstName:'Gizmo',
+      lastName:'Rubino',
+      age:10,
+      address:{
+          street:'62 E Second St.',
+          city:'Freeport',
+          state:'NY',
+          zip:11520,
+      },
+    });
+  }
+
+  addUser(user:User){
+    this.users.push(user);
+  }
 }
