03dde7b821e923349aad60b149f1d55c4e7866f2
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-24 21:49:37 -0400

    Put request to save updates

--- angular-app/src/app/components/post-form/post-form.component.ts
+++ angular-app/src/app/components/post-form/post-form.component.ts
@@ -11,6 +11,7 @@ export class PostFormComponent implements OnInit {
   @Input() currentPost:Post;
   @Input() isEdit:boolean;
   @Output() newPost:EventEmitter<Post> = new EventEmitter();
+  @Output() updatedPost:EventEmitter<Post> = new EventEmitter();
 
   constructor(private _postService: PostService){}
 
@@ -30,6 +31,10 @@ export class PostFormComponent implements OnInit {
   }
 
   updatePost(){
-    console.log(123);
+    this._postService.updatePost(this.currentPost).subscribe(post => {
+      console.log(post);
+      this.isEdit = false;
+      this.updatedPost.emit(post);
+    });
   }
 }
