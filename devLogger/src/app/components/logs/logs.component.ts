6271c2c813eb6194af14e0a1d1d61bd8d0c5db80
rubinoAM <parkcathedralgrace@gmail.com>
2019-03-27 16:27:23 -0400

    Initialized LogService

--- devLogger/src/app/components/logs/logs.component.ts
+++ devLogger/src/app/components/logs/logs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Log } from '../../models/Log';
+import { LogService } from '../../services/log.service';
 
 @Component({
   selector: 'app-logs',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs:Log[];
 
-  constructor() { }
+  constructor(private _logService: LogService){}
 
-  ngOnInit() {
+  ngOnInit(){
+    this.logs = this._logService.getLogs();
   }
-
 }
---
+++ devLogger/src/app/services/log.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Log } from '../../models/Log';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LogService {
+  logs:Log[];
+
+  constructor(){
+    this.logs = [
+      {
+        id:'1',
+        text:'Generated components',
+        date: new Date('3/27/19 16:16:23'),
+      },
+      {
+        id:'2',
+        text:'Added Bootstrap',
+        date: new Date('3/27/19 16:16:54'),
+      },
+      {
+        id:'3',
+        text:'Added logs component',
+        date: new Date('3/27/19 16:17:09'),
+      }
+    ]
+  }
+
+  getLogs(){
+    return this.logs;
+  }
+}
